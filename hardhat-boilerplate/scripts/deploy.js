351a4ac78669b058c14f7d7abd3e583f61ab19f8
Hokuto Morita <han2210mh@gmail.com>
2022-07-24 17:18:12 +0900

    [clean]スクリプトの修正

--- hardhat-boilerplate/scripts/deploy.js
+++ hardhat-boilerplate/scripts/deploy.js
@@ -1,16 +1,52 @@
+// This is a script for deploying your contracts. You can adapt it to deploy
+// yours, or create new ones.
+
 const { ethers } = require("hardhat")
 
 const main = async () => {
-    const [deployer] = await ethers.getSigners();
+    if (network.name === "hardhat") {
+        console.warn(
+          "You are trying to deploy a contract to the Hardhat Network, which" +
+            "gets automatically created and destroyed every time. Use the Hardhat" +
+            " option '--network localhost'"
+        );
+      }
 
-    console.log("Deploying contracts with the account: ", deployer.address);
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account: ", await deployer.getAddress());
     console.log("Account balance: ", (await deployer.getBalance()).toString());
 
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy();
+    await token.deployed();
 
     console.log("Token address: ", token.address);
+
+    // We also save the contract's artifacts and address in the frontend directory
+    saveFrontendFiles(token);
 }
+
+const saveFrontendFiles = (token) => {
+    const path = require("path");
+    const fs = require("fs");
+    const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        path.join(contractsDir, "contract-address.json"),
+        JSON.stringify({ Token: token.address }, undefined, 2)
+    );
+
+    const TokenArtifact = artifacts.readArtifactSync("Token");
+
+    fs.writeFileSync(
+        path.join(contractsDir, "Token.json"),
+        JSON.stringify(TokenArtifact, null, 2)
+    );
+};
 
 main()
     .then(() => process.exit(0))
