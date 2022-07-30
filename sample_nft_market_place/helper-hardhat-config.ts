41b74c358f2a503585440bf6974df3562c5c0451
Hokuto Morita <han2210mh@gmail.com>
2022-07-30 15:47:34 +0900

    [add]TypeScript

--- sample_nft_market_place/helper-hardhat-config.ts
+++ sample_nft_market_place/helper-hardhat-config.ts
@@ -1,6 +1,4 @@
-const developmentChains = ["hardhat", "localhost"]
+export const developmentChains = ["hardhat", "localhost"]
 
 // 取引(新しいスマートコントラクトの作成を含む)が確定したとみなす前にブロックチェーンに追加されなければならないブロックの数
-const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-
-module.exports = {developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS }
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6
