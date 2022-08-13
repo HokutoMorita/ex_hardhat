8b6356d7e7bcf25ff7482386ebbfa0d152f4537d
Hokuto Morita <han2210mh@gmail.com>
2022-08-13 15:21:48 +0900

    [clean]設定変更

--- sample_nft_market_place_no_moralis/hardhat.config.ts
+++ sample_nft_market_place_no_moralis/hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
+    }
+  }
 };
 
 export default config;
